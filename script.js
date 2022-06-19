83618903912bf6e7350054832a8c03dee7222529
yura20066 <yura20066@gmail.com>
2022-06-19 19:21:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    const accordion = (triggersSelector) => {
+        const btns = document.querySelectorAll(triggersSelector);
+
+        btns.forEach(btn => {
+            btn.addEventListener('click', function () {
+                this.classList.toggle('active-style');
+                this.nextElementSibling.classList.toggle('active-content');
+
+                if (this.classList.contains('active-style')) {
+                    this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+                } else {
+                    this.nextElementSibling.style.maxHeight = '0px';
+                }
+            });
+        });
+
+
+    };
+    accordion('.accordion-heading');
+
+});
